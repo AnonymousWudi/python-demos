b48e278e7bcdc1e0ffedfbae17d3856432c8200e
wudim <wudi_tju@163.com>
2016-07-11 22:17:01 +0800

    hdu 1004 finish

--- hdu_1004.cpp
+++ hdu_1004.cpp
@@ -1,14 +1,31 @@
-#include <iostream>
-
-using namespace std;
-
-struct ColorNode
-{
-    string color_name;
-    int color_time;
-};
+#include <stdio.h>
+#include <string.h>
 int main()
 {
-
-    return 0;
+    int n,i,j,num[1000];
+    int max=0,t=0;
+    char color[1000][16];
+    while(scanf("%d",&n)!=EOF)
+    {
+        if(n)
+        {
+            num[0]=0;
+            scanf("%s",color[0]);
+            for(i=1;i<n;i++)
+            {
+                num[i]=0;
+                scanf("%s",color[i]);
+                for(j=0;j<i-1;j++)
+                    if(strcmp(color[i],color[j])==0) num[i]+=1;
+            }
+            max=0;
+            t=0;
+            for(i=1;i<n;i++)
+               if(max<num[i])
+               {
+                   max=num[i];
+                   t=i;}
+            printf("%s\n",color[t]);
+        }
+    }
 }
